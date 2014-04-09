c5b004a22f2609a9287bb49a60289eeec994e70a
jakeatwork <jakedlevine2@gmail.com>
2014-04-09 10:16:50 -0700

    Update division references in views and controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -78,7 +78,7 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:name, :division, :region, :price_id)
+      params.require(:client).permit(:name, :division_id, :region, :price_id)
     end
 
     def sort_column
