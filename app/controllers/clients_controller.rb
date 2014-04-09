d4a23e9af582c04632578d0a8a910ce2ceb79f1a
jakeatwork <jakedlevine2@gmail.com>
2014-04-09 09:59:01 -0700

    Fix more merge issues

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -17,6 +17,7 @@ class ClientsController < ApplicationController
   # GET /clients/1
   # GET /clients/1.json
   def show
+    @client = Client.find(params[:id])
   end
 
   # GET /clients/new
@@ -26,11 +27,7 @@ class ClientsController < ApplicationController
 
   # GET /clients/1/edit
   def edit
-
-    @price = Client.cost
-
-
-
+    @client = Client.find(params[:id])
   end
 
   # POST /clients
