b05ce851dec997eb97432cd0c6252c6838d45d1b
Jake Levine <jakedlevine2@gmail.com>
2014-07-28 12:07:26 -0700

    Have root load clients by default

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rollout::Application.routes.draw do
 
   resources :clients
 
-  root :to => "home#index"
+  root :to => "clients#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
